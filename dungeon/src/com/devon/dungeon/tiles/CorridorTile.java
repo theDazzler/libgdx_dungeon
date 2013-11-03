023150a1a11910b01faf54e93128ff744ccfe147
Devon <divac14@yahoo.com>
2013-11-03 15:37:11 -0500

    changed tiles to be individual images rather than one large sprite sheet

--- dungeon/src/com/devon/dungeon/tiles/CorridorTile.java
+++ dungeon/src/com/devon/dungeon/tiles/CorridorTile.java
@@ -1,5 +1,8 @@
 package com.devon.dungeon.tiles;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.devon.dungeon.Graphics;
 
 public class CorridorTile extends Tile
@@ -7,7 +10,7 @@ public class CorridorTile extends Tile
 
 	public CorridorTile(int id, int xIndex, int yIndex)
 	{
-		super(id, Graphics.tiles[xIndex][yIndex]);
+		super(id, new TextureRegion(new Texture(Gdx.files.internal("data/tiles/corridor.png"))));
 	}
 
 
--- dungeon/src/com/devon/dungeon/tiles/DoorTile.java
+++ dungeon/src/com/devon/dungeon/tiles/DoorTile.java
@@ -1,5 +1,7 @@
 package com.devon.dungeon.tiles;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.devon.dungeon.Graphics;
 
@@ -8,7 +10,7 @@ public class DoorTile extends Tile
 
 	public DoorTile(int id, int xIndex, int yIndex)
 	{
-		super(id, Graphics.tiles[xIndex][yIndex]);
+		super(id, new TextureRegion(new Texture(Gdx.files.internal("data/tiles/door.png"))));
 	}
 
 	
